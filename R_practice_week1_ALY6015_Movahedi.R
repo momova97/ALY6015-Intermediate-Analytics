d096234ad92bd51286949ef8f0af95e8b26168cc
Mohammad <m.h.movahedi97@gmail.com>
2022-04-20 21:54:15 -0400

    initial commit

---
+++ R_practice_week1_ALY6015_Movahedi.R
@@ -0,0 +1,26 @@
+print('Mohammad Hossein Movahedi')
+print('R practice week 1')
+#importing and instaling libraries
+#importing and instaling libraries
+install.packages('FSA')
+install.packages('magrittr')
+install.packages('dplyr')
+install.packages('tidyr')
+install.packages('plyr')
+install.packages('tidyverse')
+install.packages('outliers')
+install.packages('ggplot2')
+install.packages('lubridate')
+
+library(FSA)
+library(magrittr)
+library(dplyr)
+library(tidyr)
+library(plyr)
+library(tidyverse)
+library(scales)
+library(lubridate)
+library(ggplot2)
+library(outliers)
+
+#Importing dataset
